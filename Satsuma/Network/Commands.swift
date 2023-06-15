308f157d382a3281fc6a4409c7a30d6a2bd4d887
Fonta1n3 <fontainedenton@googlemail.com>
2023-06-15 02:18:02 -0400

    fix: testnet now works with mempool space, custom server now works

--- Satsuma/Network/Commands.swift
+++ Satsuma/Network/Commands.swift
@@ -15,15 +15,24 @@ class Commands {
         case fee
         
         var stringValue:String {
+            
+            var rootUrl = UserDefaults.standard.object(forKey: "url") as? String ?? "https://blockstream.info"
+            
             var mempoolApiRoot = "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/api/v1"
             let mempoolApiRootClear = "https://mempool.space/api/v1"
-            var rootUrl = UserDefaults.standard.object(forKey: "url") as? String ?? "https://blockstream.info/testnet/api"
             
             let torEnabled = UserDefaults.standard.object(forKey: "torEnabled") as? Bool ?? false
-            
             if !torEnabled {
                 mempoolApiRoot = mempoolApiRootClear
             }
+            
+            if let customUrl = UserDefaults.standard.object(forKey: "customUrl") as? String, customUrl != "" {
+                rootUrl = customUrl
+            }
+            
+            // https://mempool.space/testnet/api/v1/address/tb1q2l2w6mpmr7psm
+            // https://blockstream.info/testnet/api/address/tb1qz9q0cpglp3xvuhkzsjffklyf46le889vwlujgq/utxo
+            // https://mempool.space/testnet/api/address/tb1q4kgratttzjvkxfmgd95z54qcq7y6hekdm3w56u/utxo
             
             switch self {
             case .utxo(let address):
--- Satsuma/View Controllers/BlockExplorerTableViewController.swift
+++ Satsuma/View Controllers/BlockExplorerTableViewController.swift
@@ -9,128 +9,128 @@ import UIKit
 
 class BlockExplorerTableViewController: UITableViewController, UITextFieldDelegate {
     
-    let urls:[[String:String]] = [
-        [
-            "Blockstream Testnet - Tor": "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/testnet/api"
-        ],
-        [
-            "Mempool Space - Tor": "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/api/v1"
-        ],
-        [
-            "Blockstream Testnet - Clearnet": "https://blockstream.info/testnet/api"
-        ],
-        [
-            "Mempool Space - Clearnet": "https://mempool.space/api/v1"
-        ],
-        [
-            "Custom": UserDefaults.standard.object(forKey: "customUrl") as? String ?? ""
-        ]
-    ]
+    var urls:[[String:String]] = []
+    var customUrls:[String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        urls = [
+            [
+                "mempool.space": "https://mempool.space/testnet/api"
+            ],
+            [
+                "blockstream.info": "https://blockstream.info/testnet/api"
+            ]
+        ]
+        
+        let torEnabled = UserDefaults.standard.object(forKey: "torEnabled") as! Bool
+        
+        if let network = UserDefaults.standard.object(forKey: "network") as? String {
+            if network == "testnet" {
+                if torEnabled {
+                    urls = [
+                        [
+                            "blockstream.info (via tor)": "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/testnet/api"
+                        ],
+                        [
+                            "mempool.space (via tor)": "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/testnet/api/v1"
+                        ]
+                    ]
+                }
+            } else {
+                if torEnabled {
+                    urls = [
+                        [
+                            "blockstream.info (via tor)": "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/api"
+                        ],
+                        [
+                            "mempool.space (via tor)": "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/api/v1"
+                        ]
+                    ]
+                } else {
+                    urls = [
+                        [
+                            "mempool.space": "https://mempool.space/api/v1"
+                        ],
+                        [
+                            "blockstream.info": "https://blockstream.info/api"
+                        ]
+                    ]
+                }
+            }
+        }
+        
+        let customUrl = UserDefaults.standard.object(forKey: "customUrl") as? String ?? ""
+        customUrls.append(customUrl)
     }
 
     // MARK: - Table view data source
+    
+    private enum Section: Int {
+        case standard
+        case custom
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return urls.count
+        switch section {
+        case 0:
+            return urls.count
+        case 1:
+            return 1
+        default:
+            return 0
+        }
+        
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let urlCell = tableView.dequeueReusableCell(withIdentifier: "urlCell", for: indexPath)
-        let url = UserDefaults.standard.object(forKey: "url") as? String ?? urls[2]["Blockstream Testnet - Clearnet"]
-        
-        let customUrlCell = tableView.dequeueReusableCell(withIdentifier: "customUrlCell", for: indexPath)
-        let customUrl = UserDefaults.standard.object(forKey: "customUrl") as? String ?? ""
-                
-        switch indexPath.row {
+        switch indexPath.section {
         case 0:
-            let textField = customUrlCell.viewWithTag(1) as! UITextField
-            let label = customUrlCell.viewWithTag(2) as! UILabel
-            textField.delegate = self
-            textField.text = customUrl
-            label.text = "Custom"
+            let urlCell = tableView.dequeueReusableCell(withIdentifier: "urlCell", for: indexPath)
+            let selectedUrl = UserDefaults.standard.object(forKey: "url") as? String ?? urls[2]["blockstream.info"]
+            let url = urls[indexPath.row]
             
-            if customUrl == url {
+            for (key, value) in url {
+                urlCell.textLabel!.text = key
+                
+                if value == selectedUrl {
+                    urlCell.setSelected(true, animated: true)
+                    urlCell.textLabel!.alpha = 1
+                    urlCell.accessoryType = .checkmark
+                } else {
+                    urlCell.setSelected(false, animated: true)
+                    urlCell.textLabel!.alpha = 0.2
+                    urlCell.accessoryType = .none
+                }
+            }
+            
+            return urlCell
+            
+        case 1:
+            let customUrlCell = tableView.dequeueReusableCell(withIdentifier: "customUrlCell", for: indexPath)
+            let customUrl = customUrls[0]
+            let textField = customUrlCell.viewWithTag(1) as! UITextField
+            textField.text = customUrl
+            let selectedUrl = UserDefaults.standard.object(forKey: "url") as? String ?? urls[2]["blockstream.info"]
+            
+            if customUrl == selectedUrl {
                 customUrlCell.setSelected(true, animated: true)
-                label.alpha = 1
+                customUrlCell.textLabel!.alpha = 1
                 customUrlCell.accessoryType = .checkmark
             } else {
                 customUrlCell.setSelected(false, animated: true)
-                label.alpha = 0.2
+                customUrlCell.textLabel!.alpha = 0.2
                 customUrlCell.accessoryType = .none
             }
             
             return customUrlCell
-            
-        case 1:
-            urlCell.textLabel!.text = "Blockstream Testnet - Tor"
-            
-            if urls[0]["Blockstream Testnet - Tor"] == url {
-                urlCell.setSelected(true, animated: true)
-                urlCell.textLabel!.alpha = 1
-                urlCell.accessoryType = .checkmark
-            } else {
-                urlCell.setSelected(false, animated: true)
-                urlCell.textLabel!.alpha = 0.2
-                urlCell.accessoryType = .none
-            }
-            
-            return urlCell
-            
-        case 2:
-            urlCell.textLabel!.text = "Mempool Space - Tor"
-            
-            if urls[1]["Mempool Space - Tor"] == url {
-                urlCell.setSelected(true, animated: true)
-                urlCell.textLabel!.alpha = 1
-                urlCell.accessoryType = .checkmark
-            } else {
-                urlCell.setSelected(false, animated: true)
-                urlCell.textLabel!.alpha = 0.2
-                urlCell.accessoryType = .none
-            }
-            
-            return urlCell
-            
-        case 3:
-            urlCell.textLabel!.text = "Blockstream Testnet - Clearnet"
-            
-            if urls[2]["Blockstream Testnet - Clearnet"] == url {
-                urlCell.setSelected(true, animated: true)
-                urlCell.textLabel!.alpha = 1
-                urlCell.accessoryType = .checkmark
-            } else {
-                urlCell.setSelected(false, animated: true)
-                urlCell.textLabel!.alpha = 0.2
-                urlCell.accessoryType = .none
-            }
-            
-            return urlCell
-            
-        case 4:
-            urlCell.textLabel!.text = "Mempool Space - Clearnet"
-            
-            if urls[3]["Mempool Space - Clearnet"] == url {
-                urlCell.setSelected(true, animated: true)
-                urlCell.textLabel!.alpha = 1
-                urlCell.accessoryType = .checkmark
-            } else {
-                urlCell.setSelected(false, animated: true)
-                urlCell.textLabel!.alpha = 0.2
-                urlCell.accessoryType = .none
-            }
-            
-            return urlCell
             
         default:
             return blankCell()
@@ -138,21 +138,18 @@ class BlockExplorerTableViewController: UITableViewController, UITextFieldDelega
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
+        switch indexPath.section {
         case 0:
-            UserDefaults.standard.set(urls[4]["Custom"], forKey: "url")
+            let url = urls[indexPath.row]
+            for (_, value) in url {
+                UserDefaults.standard.set(value, forKey: "url")
+            }
             
         case 1:
-            UserDefaults.standard.set(urls[0]["Blockstream Testnet - Tor"], forKey: "url")
-            
-        case 2:
-            UserDefaults.standard.set(urls[1]["Mempool Space - Tor"], forKey: "url")
-            
-        case 3:
-            UserDefaults.standard.set(urls[2]["Blockstream Testnet - Clearnet"], forKey: "url")
-            
-        case 4:
-            UserDefaults.standard.set(urls[3]["Mempool Space - Clearnet"], forKey: "url")
+            if customUrls[0] != "" {
+                UserDefaults.standard.set(customUrls[0], forKey: "customUrl")
+                UserDefaults.standard.set(customUrls[0], forKey: "url")
+            }
             
         default:
             break
@@ -160,6 +157,52 @@ class BlockExplorerTableViewController: UITableViewController, UITextFieldDelega
         
         tableView.reloadData()
     }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 1:
+            let cell = tableView.cellForRow(at: indexPath)!
+            cell.setSelected(false, animated: true)
+            cell.textLabel!.alpha = 0.2
+            cell.accessoryType = .none
+            
+        default:
+            break
+        }
+        
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = UIColor.clear
+        header.frame = CGRect(x: 0, y: 0, width: view.frame.size.width - 32, height: 50)
+
+        let textLabel = UILabel()
+        textLabel.textAlignment = .left
+        textLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        textLabel.frame = CGRect(x: 16, y: 0, width: 300, height: 50)
+
+        if let section = Section(rawValue: section) {
+            textLabel.text = headerName(for: section)
+        }
+
+        header.addSubview(textLabel)
+        return header
+    }
+    
+    private func headerName(for section: Section) -> String {
+        switch section {
+        case .standard:
+            return "Public servers"
+        case .custom:
+            return "Private server"
+        }
+    } 
     
     private func blankCell() -> UITableViewCell {
         let cell = UITableViewCell()
